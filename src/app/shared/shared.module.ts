31d04a6f3c715586215e806ca4e6969ed990d47e
Gustavo Munhoz <gustavo_munhoz@yahoo.com>
2022-06-27 19:06:53 -0300

    adjust schedules screen

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule,} from '@angular/material/toolbar';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
 
-import {MatIconModule} from '@angular/material/icon'
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
-
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
@@ -26,9 +25,21 @@ import { FooterComponent } from './footer/footer.component';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatDatepickerModule
-
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
   ],
-  exports: [HeaderComponent,FooterComponent,MatIconModule, MatButtonModule,MatInputModule,FormsModule,ReactiveFormsModule,MatDatepickerModule]
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
