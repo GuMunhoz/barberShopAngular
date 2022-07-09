4905699177d59688259653d92e7b30580109de15
Gustavo Munhoz <gustavo_munhoz@yahoo.com>
2022-07-09 18:20:39 -0300

    adjust my schedule screen

--- src/app/security/my-schedules/my-schedules.component.ts
+++ src/app/security/my-schedules/my-schedules.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  hour: string;
+  nameBarber: string;
+  service: string;
+  status: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { hour: '05/07/2022 09:00', nameBarber: 'Bruno' ,service: 'Corte + barba',status:'pending'},
+  { hour: '15/06/2022 16:00', nameBarber: 'Bruno', service: 'Corte + barba' , status:'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+  { hour: '02/06/2022 11:00', nameBarber: 'Felipe',service:'Barba' , status: 'done'},
+];
+
 @Component({
   selector: 'app-my-schedules',
   templateUrl: './my-schedules.component.html',
@@ -7,6 +25,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySchedulesComponent implements OnInit {
 
+  displayedColumns: string[] = ['hour', 'nameBarber','service','status'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
