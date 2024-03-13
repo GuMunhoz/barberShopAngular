1a9adc997c06d28ca2f190d66b393ff8c2def73f
Gustavo Munhoz Santos <gmunhoz@premiersp.local>
2024-03-13 18:03:00 -0300

    update project

--- src/app/security/admin/admin.component.ts
+++ src/app/security/admin/admin.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  containers: object [] = [{ containerTitle: 'Dados pessoais' },{containerTitle: 'Alterar senha'}];
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.containers);
   }
 
+  getNavigate() {
+    if (this.containers[0]) {
+      console.log('Dados pessoais')
+    }
+    // if(this.containers[1]) {
+    //   console.log('Alterar senha')
+    // }
+  }
 }
